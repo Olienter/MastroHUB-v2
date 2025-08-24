74389f8f3cf5148c002278687bd69b032ccbb1b1
Olienter <oliebodnar@gmail.com>
2025-08-25 00:51:37 +0200

    CI TRIGGER: Playwright webServer config + watchdog test updated - should trigger Adaptive Enforcement CI

--- public/huge-test.js
+++ public/huge-test.js
@@ -1,4 +1,4 @@
 /* REDTEAM: will be reverted */
-/* CI TRIGGER: explicit --no-frozen-lockfile to override pnpm action default */
+/* CI TRIGGER: Playwright webServer config + watchdog test updated */
 const pad = 'X'.repeat(400_000);
 console.log('[WatchdogTest] huge-test bytes:', pad.length);
